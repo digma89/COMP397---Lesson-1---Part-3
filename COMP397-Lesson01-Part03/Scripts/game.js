2b8de365ad301c1e0dde0bbf678ce9e854ae1239
digma89 <digma89@hotmail.com>
2015-05-05 10:52:23 -0400

    Initial Commit

---
+++ COMP397-Lesson01-Part03/Scripts/game.js
@@ -0,0 +1,9 @@
+﻿function init() {
+    console.log("Game is Running...");
+}
+/*
+//anonymous function
+var init = function () {
+}
+*/
+//# sourceMappingURL=game.js.map
---
+++ COMP397-Lesson01-Part03/Scripts/game.ts
@@ -0,0 +1,12 @@
+﻿
+
+function init() {
+    console.log("Game is Running...");
+}
+
+
+/*
+//anonymous function 
+var init = function () {
+}
+*/
