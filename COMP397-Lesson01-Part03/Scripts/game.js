3d8bad50277bd8d553fff15ec0bf1667175cc6a9
digma89 <digma89@hotmail.com>
2015-05-05 11:25:20 -0400

    add create js to proyect

--- COMP397-Lesson01-Part03/Scripts/game.js
+++ COMP397-Lesson01-Part03/Scripts/game.js
@@ -1,5 +1,28 @@
-﻿function init() {
-    console.log("Game is Running...");
+﻿/// <reference path="typings/easeljs/easeljs.d.ts" />
+var stage;
+var canvas = document.getElementById("canvas");
+
+function init() {
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    stage.update();
+}
+
+function main() {
+    console.log("Game is running");
+
+    var helloText = new createjs.Text("Hello world!", "40px Consolas", "#000000");
+
+    helloText.x = 30;
+    helloText.y = 200;
+
+    stage.addChild(helloText);
 }
 /*
 //anonymous function
--- COMP397-Lesson01-Part03/Scripts/game.ts
+++ COMP397-Lesson01-Part03/Scripts/game.ts
@@ -1,9 +1,33 @@
-﻿
+﻿/// <reference path="typings/easeljs/easeljs.d.ts" />
+
+
+var stage: createjs.Stage;
+var canvas = document.getElementById("canvas");
+
 
 function init() {
-    console.log("Game is Running...");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+
+    main();
 }
 
+function gameLoop() {
+
+    stage.update();
+}
+
+function main() {
+    console.log("Game is running");
+
+    var helloText: createjs.Text = new createjs.Text("Hello world!", "40px Consolas", "#000000");
+
+    helloText.x = 30;
+    helloText.y = 200;
+
+    stage.addChild(helloText);
+}
 
 /*
 //anonymous function 
